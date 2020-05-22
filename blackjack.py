f4626a5b682481d3142d6ff17920608af63163c8
jryan95 <64394878+jryan95@users.noreply.github.com>
2020-05-21 20:43:10 -0500

    commit on 'master'

---
+++ blackjack.py
@@ -0,0 +1,11 @@
+# main blackjack application
+
+__author__ = "Jon Ryan"
+__version__ = "0.0.1"
+
+def main():
+    print('Welcome to Blackjack v{} by {}'.format
+    (__version__, __author__))
+
+if __name__ == "__main__":
+    main()
