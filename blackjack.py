733340358acd8fc16ac33a3db780f912d44f3619
jryan95 <64394878+jryan95@users.noreply.github.com>
2020-05-22 21:15:55 -0500

    figuring out how to print both player cards

--- blackjack.py
+++ blackjack.py
@@ -6,19 +6,19 @@ __version__ = "0.0.1"
 import random
 
 CARDS = {
-    0 : "A",
-    1 : "1",
-    2 : "2",
-    3 : "3",
-    4 : "4",
-    5 : "5",
-    6 : "6",
-    7 : "7",
-    8 : "8",
-    9 : "9",
-    10 : "J",
-    11 : "Q",
-    12 : "K"
+    0 : ("A", 1, 11),
+    1 : ("1", 1),
+    2 : ("2", 2),
+    3 : ("3", 3),
+    4 : ("4", 4),
+    5 : ("5", 5),
+    6 : ("6", 6),
+    7 : ("7", 7),
+    8 : ("8", 8),
+    9 : ("9", 9),
+    10 : ("J", 10),
+    11 : ("Q", 10),
+    12 : ("K", 10)
 }
 
 class Blackjack():
@@ -26,17 +26,18 @@ class Blackjack():
         self._player1 = ""
         self._player2 = ""
         self._player1cards = []
-        self._player2cards = []
         self._dealercards = []
-    
-    def prevwinner(self):
-        pass
 
-    def dealhand(self, player1, player2=None):
+    def dealhand(self, player1=None, player2=None):
         while len(self._player1cards) != 2:
             self._player1cards.append(random.choice(CARDS))
             if len(self._player1cards) == 2:
-                print("You have ", self._player1cards)
+                print("You have ", self._player1cards[-1][0])
+        while len(self._dealercards) != 2:
+            self._dealercards.append(random.choice(CARDS))
+            if len(self._dealercards) == 2:
+                print("Dealer has X &", self._dealercards[-1][0])
+        return
 
     def hit(self):
         pass
@@ -48,33 +49,17 @@ def main():
     print('Welcome to Blackjack v{} by {}'.format
     (__version__, __author__))
     print("**" * 20)
-    players = int(input("How many people are playing?: "))
-    if players > 2:
-        print("Error, can't handle more than 2 players in version {}".format(__version__))
-    elif players == 2:
-        player1 = input("Player 1, please input your name: ")
-        player2 = input("Player 2, please input your name: ")
-        two_player = True
-    else:
-        player1 = input("Player 1, please input your name: ")
-        two_player = False
-        print("Prepare to play against the dealer.")
-        print("**" * 20)
     
-    
-    first_game = True
+    # Used for checking if it is first game or not, if not, previous winner goes first
+    # first_game = True
     game = Blackjack()
     # Main game loop
     while True:
-        if not first_game:
-            previous_winner = game.prevwinner()
-        
         print("Dealing hand...")
+        game.dealhand()
         print("**" * 20)
-        if two_player:
-            game.dealhand(player1, player2)
-        else:
-            game.dealhand(player1)
+
+
 
         break
 
