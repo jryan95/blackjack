9f0f67a32425b1c85f52c53d764f6955d8cf58e7
jryan95 <64394878+jryan95@users.noreply.github.com>
2020-05-22 23:48:41 -0500

    update, goodnight

--- blackjack.py
+++ blackjack.py
@@ -6,7 +6,7 @@ __version__ = "0.0.1"
 import random
 
 CARDS = {
-    0 : ("A", 1, 11),
+    0 : ("A", 11),
     1 : ("1", 1),
     2 : ("2", 2),
     3 : ("3", 3),
@@ -31,6 +31,7 @@ class Blackjack():
         self._card2 = 0
         self._dealercard1 = 0
         self._dealercard2 = 0
+        self._inhandvalue = 0
 
     def dealhand(self, player1=None, player2=None):
         while len(self._playercards) != 2:
@@ -38,18 +39,38 @@ class Blackjack():
             self._card1 = self._playercards[-1][0]
             self._card2 = self._playercards[0][0]
             if len(self._playercards) == 2:
-                print("You have ", self._card1, " &", self._card2)
+                print("You have", self._card1, "&", self._card2)
         while len(self._dealercards) != 2:
             self._dealercards.append(random.choice(CARDS))
+            self._dealercard1 = self._dealercards[-1][0]
+            self._dealercard2 = self._dealercards[0][0]
             if len(self._dealercards) == 2:
                 # [-1] gets the last element in the iterable
                 print("Dealer has X &", self._dealercards[-1][0])
         return
     
-    def checkhands(self):
-        print(self._card1, self._card2)
-
+    def checkhand(self, player=None, dealer=None):
+        self._card1 = self._playercards[-1][1]
+        self._card2 = self._playercards[0][1]
+        playerhand = sum([self._card1, self._card2])
+        # checking player cards
+        if playerhand > 21:
+            if self._card1 or self._card2 == 11:
+                self._inhandvalue = playerhand - 10
+                print("You have a total of", self._inhandvalue, ", hit or stay?")
+                # option = input("You have a total of", self._inhandvalue, ", hit or stay?")
+            else:
+                print("You busted!")
+        elif playerhand == 21:
+            print("Blackjack!")
+        else:
+            self._inhandvalue = playerhand
+            print("You have a total of", self._inhandvalue, ", hit or stay?")
+        
     def hit(self):
+        pass
+
+    def stay(self):
         pass
 
 
@@ -68,9 +89,17 @@ def main():
         print("Dealing hand...")
         game.dealhand()
         print("**" * 20)
-        game.checkhands()
-
-
+        game.checkhand()
+        option = input(">> ")
+        if option.lower() is 'hit':
+            game.hit()
+        elif option.lower() is 'stay':
+            game.stay()
+        else:
+            print("Invalid option.")
+            game.checkhand()
+        
+        break
 
 
 if __name__ == "__main__":
